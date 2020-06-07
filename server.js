84e2f6fac072246e678268b85282b6cbcbf97e41
barbaramistrz <bstolarz@gmail.com>
2020-06-07 14:42:33 +0200

    check

--- server.js
+++ server.js
@@ -3,14 +3,26 @@
 
 const express = require('express');
 const http = require('http');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 const httpServer = http.Server(app);
+
  
 app.get('/api', (req, res) => {
     res.json({status: 'server works!'})
+});
+
+app.get('/api/posts', (req, res) => {
+    res.json({posts: []})
+})
+// serve static react app
+app.use('/', express.static(path.resolve(path.dirname(''), './client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(path.dirname(''), './client/build/index.html'));
 });
 
 httpServer.listen(PORT, () => {console.log('Server listening on port', PORT)} )
